891f5726233163c689392c3f264d4e98d085c0fe
Simon Emanuel Schmid <simon@schmid.io>
2019-04-08 21:21:55 +0200

    Add header image

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import css from "./index.css";
 
 const Home = () => (
   <div className={css.container}>
+    <img src="/static/titile.jpg" alt="Postcard by Veli &amp; Amos" />
     <h1>This is not a commercial</h1>
   </div>
 );
