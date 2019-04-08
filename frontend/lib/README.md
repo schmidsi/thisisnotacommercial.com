12f068bec499fa562c64d106a3f5749e4015daab
Simon Emanuel Schmid <simon@schmid.io>
2019-04-08 22:13:19 +0200

    Quick and dirty data fetching

---
+++ frontend/lib/README.md
@@ -0,0 +1,1 @@
+From: https://github.com/softspider/zeit-now-next-typescript-graphql-apollo
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 import css from "./index.css";
 
 const Home = () => (
-  <div className={css.container}>
-    <img src="/static/titile.jpg" alt="Postcard by Veli &amp; Amos" />
-    <h1>This is not a commercial</h1>
-  </div>
+  <Query
+    query={gql`
+      {
+        currentPrice
+      }
+    `}
+  >
+    {(result: any) => {
+      const { data } = result;
+
+      return (
+        <div className={css.container}>
+          <img src="/static/titile.jpg" alt="Postcard by Veli &amp; Amos" />
+          <h1>This is not a commercial</h1>
+          <div>Current price: {data.currentPrice}€</div>
+        </div>
+      );
+    }}
+  </Query>
 );
 
 export default Home;
