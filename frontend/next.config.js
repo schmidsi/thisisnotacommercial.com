837726ea2ff5f4d722ceb92a1d53c36524f7fe04
Simon Emanuel Schmid <simon@schmid.io>
2019-04-08 21:14:03 +0200

    CSS modules setup

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,2 +1,12 @@
 const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript();
+const withCSS = require("@zeit/next-css");
+
+module.exports = withTypescript(
+  withCSS({
+    cssModules: true,
+    sourceMap: true,
+    cssLoaderOptions: {
+      localIdentName: "[local]___[hash:base64:5]"
+    }
+  })
+);
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Home = () => <h1>This is not a commercial</h1>;
+import css from "./index.css";
+
+const Home = () => (
+  <div className={css.container}>
+    <h1>This is not a commercial</h1>
+  </div>
+);
 
 export default Home;
