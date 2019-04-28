b548463faff0130aaf01a0d50a660e75b57be60e
Simon Emanuel Schmid <simon@schmid.io>
2019-04-28 10:15:41 +0200

    addCartProduct failes because of auth

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,120 +6,143 @@ import * as R from "ramda";
 
 import CurrentPrice from "../queries/CurrentPrice.gql";
 import LoginAsGuest from "../queries/LoginAsGuest.gql";
+import AddCartProduct from "../queries/AddCartProduct.gql";
 
 import css from "./index.css";
 
 const Home = () => (
-  <div className={css.container}>
-    <img src="/static/titile.jpg" alt="Postcard by Veli &amp; Amos" />
-    <h1>This is not a commercial</h1>
+  <Query query={CurrentPrice}>
+    {(result: any) => {
+      const price = R.path(
+        ["data", "product", "simulatedPrice", "price", "amount"],
+        result
+      );
 
-    <Query query={CurrentPrice}>
-      {(result: any) => {
-        const { data } = result;
+      const productId = R.path(["data", "product", "_id"], result);
 
-        return (
-          <div>
-            Current price: {data.product.simulatedPrice.price.amount / 100}€
-          </div>
-        );
-      }}
-    </Query>
+      return (
+        <div className={css.container}>
+          <img src="/static/titile.jpg" alt="Postcard by Veli &amp; Amos" />
+          <h1>This is not a commercial</h1>
+          <div>Current price: {price / 100}€</div>
 
-    <h1>Order</h1>
-    <ApolloConsumer>
-      {client => (
-        <Formik
-          initialValues={{
-            name: "asdf",
-            address: "asdf",
-            zipCity: "asdf",
-            country: "Switzerland",
-            email: "asdf@asdf.ch",
-            message: ""
-          }}
-          validationSchema={yup.object().shape({
-            name: yup.string().required(),
-            address: yup.string().required(),
-            zipCity: yup.string().required(),
-            country: yup.string().required(),
-            email: yup
-              .string()
-              .email()
-              .required(),
-            message: yup.string()
-          })}
-          onSubmit={async (values, { setSubmitting }) => {
-            console.log(values, client);
+          <h1>Order</h1>
+          <ApolloConsumer>
+            {client => (
+              <Formik
+                initialValues={{
+                  name: "asdf",
+                  address: "asdf",
+                  zipCity: "asdf",
+                  country: "Switzerland",
+                  email: "asdf@asdf.ch",
+                  message: ""
+                }}
+                validationSchema={yup.object().shape({
+                  name: yup.string().required(),
+                  address: yup.string().required(),
+                  zipCity: yup.string().required(),
+                  country: yup.string().required(),
+                  email: yup
+                    .string()
+                    .email()
+                    .required(),
+                  message: yup.string()
+                })}
+                onSubmit={async (values, { setSubmitting }) => {
+                  console.log(values, client);
 
-            const loginAsGuestResult = await client.mutate({
-              mutation: LoginAsGuest
-            });
-            const token = R.path(
-              ["data", "loginAsGuest", "token"],
-              loginAsGuestResult
-            );
+                  const loginAsGuestResult = await client.mutate({
+                    mutation: LoginAsGuest
+                  });
 
-            console.log(token);
+                  const token = R.path(
+                    ["data", "loginAsGuest", "token"],
+                    loginAsGuestResult
+                  );
 
-            setSubmitting(false);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <label>
-                <h2 className={css.label}>Name</h2>
-                <ErrorMessage name="name" component="div" />
-                <Field type="string" name="name" className={css.field} />
-              </label>
+                  console.log(token);
 
-              <label>
-                <h2 className={css.label}>Address</h2>
-                <ErrorMessage name="address" component="div" />
-                <Field type="string" name="address" className={css.field} />
-              </label>
+                  const addCartProductResult = await client.mutate({
+                    mutation: AddCartProduct,
+                    variables: { productId }
+                  });
 
-              <label>
-                <h2 className={css.label}>ZIP / City</h2>
-                <ErrorMessage name="zipCity" component="div" />
-                <Field type="string" name="zipCity" className={css.field} />
-              </label>
+                  console.log(addCartProductResult);
 
-              <label>
-                <h2 className={css.label}>Country</h2>
-                <ErrorMessage name="country" component="div" />
-                <Field type="string" name="country" className={css.field} />
-              </label>
-
-              <label>
-                <h2 className={css.label}>Email</h2>
-                <ErrorMessage name="email" component="div" />
-                <Field type="email" name="email" className={css.field} />
-              </label>
-
-              <label>
-                <h2 className={css.label}>Message (optional)</h2>
-                <ErrorMessage name="message" component="div" />
-                <Field
-                  component="textarea"
-                  name="message"
-                  className={css.field}
-                />
-              </label>
-
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className={css.button}
+                  setSubmitting(false);
+                }}
               >
-                Submit
-              </button>
-            </Form>
-          )}
-        </Formik>
-      )}
-    </ApolloConsumer>
-  </div>
+                {({ isSubmitting }) => (
+                  <Form>
+                    <label>
+                      <h2 className={css.label}>Name</h2>
+                      <ErrorMessage name="name" component="div" />
+                      <Field type="string" name="name" className={css.field} />
+                    </label>
+
+                    <label>
+                      <h2 className={css.label}>Address</h2>
+                      <ErrorMessage name="address" component="div" />
+                      <Field
+                        type="string"
+                        name="address"
+                        className={css.field}
+                      />
+                    </label>
+
+                    <label>
+                      <h2 className={css.label}>ZIP / City</h2>
+                      <ErrorMessage name="zipCity" component="div" />
+                      <Field
+                        type="string"
+                        name="zipCity"
+                        className={css.field}
+                      />
+                    </label>
+
+                    <label>
+                      <h2 className={css.label}>Country</h2>
+                      <ErrorMessage name="country" component="div" />
+                      <Field
+                        type="string"
+                        name="country"
+                        className={css.field}
+                      />
+                    </label>
+
+                    <label>
+                      <h2 className={css.label}>Email</h2>
+                      <ErrorMessage name="email" component="div" />
+                      <Field type="email" name="email" className={css.field} />
+                    </label>
+
+                    <label>
+                      <h2 className={css.label}>Message (optional)</h2>
+                      <ErrorMessage name="message" component="div" />
+                      <Field
+                        component="textarea"
+                        name="message"
+                        className={css.field}
+                      />
+                    </label>
+
+                    <button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className={css.button}
+                    >
+                      Submit
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+            )}
+          </ApolloConsumer>
+        </div>
+      );
+    }}
+  </Query>
 );
 
 export default Home;
