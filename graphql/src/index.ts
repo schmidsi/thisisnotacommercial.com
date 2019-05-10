23364e63fad7015e08b3c166b7f3fae67e5e7679
Simon Emanuel Schmid <simon@schmid.io>
2019-05-10 09:40:28 +0200

    Remove old graphql code

--- graphql/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { GraphQLServer } from "graphql-yoga";
-
-import { schema } from "./schema";
-
-const server = new GraphQLServer({ schema });
-
-server.start(() => console.log("Server is running on localhost:4000"));
