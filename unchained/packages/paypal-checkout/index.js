0da95b820b9a560def4bd2d9d068de6ef223be1c
Simon Emanuel Schmid <simon@schmid.io>
2019-06-07 09:19:54 +0200

    Paypal processing server side

--- unchained/packages/paypal-checkout/index.js
+++ unchained/packages/paypal-checkout/index.js
@@ -59,52 +59,23 @@ class PaypalCheckout extends PaymentAdapter {
       const request = new checkoutNodeJssdk.orders.OrdersGetRequest(orderID);
       const order = await payPalClient.client().execute(request);
 
-      console.log('PAYPAL ORDER', JSON.stringify(order.result, 2, null));
-
       const pricing = this.context.order.pricing();
+      const ourTotal = (pricing.total().amount / 100).toFixed(2);
+      const paypalTotal = order.result.purchase_units[0].amount.value;
+
+      if (ourTotal === paypalTotal) {
+        return order;
+      }
+
+      console.log('PAYPAL ORDER', JSON.stringify(order.result, null, 2));
       console.log('OUR ORDER', this.context.order);
       console.log('OUR PRICE', pricing);
 
-      throw new Error('Blub');
+      throw new Error(`Payment misatch`);
     } catch (e) {
       console.error(e);
+      throw new Error(e);
     }
-
-    // if (!paypalPaymentMethodNonce)
-    //   throw new Error(
-    //     'You have to provide paypalPaymentMethodNonce in paymentContext'
-    //   );
-    // const braintree = require('braintree'); // eslint-disable-line
-    // const gateway = this.gateway(braintree);
-    // const address = this.context.order.billingAddress || {};
-    // const pricing = this.context.order.pricing();
-    // const rounded = Math.round(pricing.total().amount / 10 || 0) * 10;
-    // const saleRequest = {
-    //   amount: `${rounded / 100}.00`,
-    //   // merchantAccountId: this.context.order.currency,
-    //   paymentMethodNonce: paypalPaymentMethodNonce,
-    //   orderId: this.context.order.orderNumber || this.context.order._id,
-    //   shipping: {
-    //     firstName: address.firstName,
-    //     lastName: address.lastName,
-    //     company: address.company,
-    //     streetAddress: address.addressLine,
-    //     extendedAddress: address.addressLine2,
-    //     locality: address.city,
-    //     region: address.regionCode,
-    //     postalCode: address.postalCode,
-    //     countryCodeAlpha2: address.countryCode
-    //   },
-    //   options: {
-    //     submitForSettlement: true
-    //   }
-    // };
-    // const result = await gateway.transaction.sale(saleRequest);
-    // if (result.success) {
-    //   return result;
-    // }
-    // this.log(saleRequest);
-    // throw new Error(result.message);
   }
 }
 
