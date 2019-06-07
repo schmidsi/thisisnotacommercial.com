c1ca4c93947fbd9329c55f72bbde42e49f17354a
Simon Emanuel Schmid <simon@schmid.io>
2019-06-07 17:46:09 +0200

    PaintNumber

---
+++ frontend/components/PaintNumber.tsx
@@ -0,0 +1,45 @@
+import * as R from "ramda";
+
+const PaintNumber = ({
+  children,
+  euro = false,
+  style = {},
+  className = ""
+}) => {
+  const chars = children.toString().split("");
+
+  return (
+    <div style={style} className={"root " + className}>
+      {chars.map(char => (
+        <div
+          className="img"
+          style={{ backgroundImage: `url(/static/chars/${char}.png)` }}
+        />
+      ))}
+      {euro ? (
+        <div
+          className="img"
+          style={{ backgroundImage: `url(/static/chars/eur.png)` }}
+        />
+      ) : (
+        ""
+      )}
+      <style jsx>{`
+        .root {
+          white-space: nowrap;
+        }
+
+        .img {
+          display: inline-block;
+          width: 1em;
+          height: 1em;
+          background-size: contain;
+          background-repeat: no-repeat;
+          background-position: center bottom;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default PaintNumber;
