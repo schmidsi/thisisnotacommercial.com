637a067a473aba54a14ba0b883c38cee8eaf72e7
Simon Emanuel Schmid <simon@schmid.io>
2019-11-08 17:43:36 +0100

    Fix thankyouz

--- frontend/pages/thankyou.tsx
+++ frontend/pages/thankyou.tsx
@@ -1,27 +1,21 @@
-import { Query } from 'react-apollo';
-
 import ShareButtons from '../components/ShareButtons/index';
-import CurrentOrder from '../queries/CurrentOrder.gql';
 
 import css from './main.css';
 
-const ThankYou = () => (
-  <Query query={CurrentOrder}>
-    {result => (
-      <div className={css.container} key="main">
-        <img
-          className={css.center}
-          src="/static/thank-you.png"
-          alt="Thank you for your order!"
-        />
-        <img
-          src="/static/send-us-a-pic.png"
-          alt="Please send us a picture when you get it!"
-        />
-        <ShareButtons />
-      </div>
-    )}
-  </Query>
-);
-
+const ThankYou = () => {
+  return (
+    <div className={css.container} key="main">
+      <img
+        className={css.center}
+        src="/static/thank-you.png"
+        alt="Thank you for your order!"
+      />
+      <img
+        src="/static/send-us-a-pic.png"
+        alt="Please send us a picture when you get it!"
+      />
+      <ShareButtons />
+    </div>
+  );
+};
 export default ThankYou;
