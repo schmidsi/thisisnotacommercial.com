49057acb387fa0b7d33d43ab54e55b7acfddae2a
Simon Emanuel Schmid <simon@schmid.io>
2019-11-21 16:32:42 +0100

    $ENV

--- frontend/lib/withData.ts
+++ frontend/lib/withData.ts
@@ -1,10 +1,18 @@
 import { withData } from 'next-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
+const GRAPHQL_ENDPOINT =
+  process.env.GRAPHQL_ENDPOINT ||
+  publicRuntimeConfig.GRAPHQL_ENDPOINT ||
+  'http://localhost:4010/graphql';
 
 const httpLink = new HttpLink({
   credentials: 'same-origin',
-  uri: process.env.GRAPHQL_ENDPOINT || 'http://localhost:4010/graphql'
+  uri: process.env.GRAPHQL_ENDPOINT
 });
 
 const authLink = setContext((_, { headers }) => {
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,18 @@
-const withPlugins = require("next-compose-plugins");
-const typescript = require("@zeit/next-typescript");
-const css = require("@zeit/next-css");
-const graphql = require("next-plugin-graphql");
+const withPlugins = require('next-compose-plugins');
+const typescript = require('@zeit/next-typescript');
+const css = require('@zeit/next-css');
+const graphql = require('next-plugin-graphql');
 
 module.exports = withPlugins([typescript, css, graphql], {
   cssModules: true,
   sourceMap: true,
   cssLoaderOptions: {
-    localIdentName: "[local]___[hash:base64:5]"
+    localIdentName: '[local]___[hash:base64:5]'
   },
   env: {
+    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT
+  },
+  publicRuntimeConfig: {
     GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT
   }
 });
