7e30ded519c54de91810b8a7ff14f46679719ccd
Pascal Kaufmann <kaufmae@me.com>
2020-02-26 10:21:40 +0100

    feat: remove swarmpit deploy script (use swarmpit’s auto polling feature for this)

--- swarmpit_deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "curl -X POST https://swarmpit.ucc.dev/api/stacks/${1}/redeploy -H \"Authorization: XXXXX\""
-curl -X POST https://swarmpit.ucc.dev/api/stacks/${1}/redeploy -H "Authorization: ${2}"
